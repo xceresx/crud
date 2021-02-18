3d26d12dc7f3114bbe16a11915953198b5129e26
Paulo César Hernández Naranjo <xceresx@users.noreply.github.com>
2021-02-17 19:55:58 -0500

    crear tabala dinamica

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import React from 'react'
-
-
+import React, {useState}  from 'react'
+import {isEmpty} from 'lodash'
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask]= useState("")
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (e) => {
+    e.preventDefault()
+    if (isEmpty(task)) { 
+      console.log("Task empty")
+      return
+    }
+    
+    const newTask={
+      id:shortid.generate(),
+      task
+    }
+
+    setTasks([...tasks,newTask])
+    setTask("")
+
+  } 
+
   return (
     <div className="container mt-5">
-      <h1>Tareas</h1>
+      <h1>Tasks</h1>
       <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Tasks List</h4>
+            <ul className="list-group">
+            
+              {
+                tasks.map((task)=> ( 
+                <li className="list-group-item" key={task.id}>
+                  <span className="lead">{task.name}</span>
+                  <button className="btn btn-danger btn-sm float-right mx-2">Delete</button>
+                  <button className="btn btn-warning btn-sm float-right">Edit</button>
+                </li>
+                ))
+              }
+
+            </ul>
+        </div>
+        <div className="col-4">
+        <h4 className="text-center">Form</h4>
+          <form onSubmit={addTask}>  
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Enter the task..."
+              onChange={(text)=> setTask(text.target.value)}
+              value={task}
+            />
+            <button 
+              className="btn btn-dark btn-block"
+              type="submit"
+            > Add            
+            </button>
+          </form>
+        </div>
+      </div>
+
     </div>
   )
 }
